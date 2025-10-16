ad38ec77f6e27a5437133b5903c651f1f003ee08
Artelhin <artelhin@gmail.com>
2025-10-16 17:51:02 +0300

    Run go mod tidy & go mod vendor

---
+++ vendor/github.com/Edge-Center/edgecenter-dns-sdk-go/README.md
@@ -0,0 +1,3 @@
+# EdgeCenter DNS API Golang SDK
+
+The purpose of this project is to cover EdgeCenter DNS API methods related with EdgeCenter Terraform plugin (https://github.com/Edge-Center/terraform-provider-edgecenter).
---
+++ vendor/github.com/Edge-Center/edgecenter-dns-sdk-go/client_dto.go
@@ -0,0 +1,371 @@
+package dnssdk
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+// Meta dto to read meta
+type Meta struct {
+	Failover *FailoverMeta `json:"failover,omitempty"`
+}
+
+// ListZones dto to read list of zones from API
+type ListZones struct {
+	Zones []Zone `json:"zones"`
+}
+
+// Zone dto to read info from API
+type Zone struct {
+	Name    string       `json:"name"`
+	Records []ZoneRecord `json:"records"`
+}
+
+// AddZone dto to create new zone
+type AddZone struct {
+	Name string `json:"name"`
+}
+
+// CreateResponse dto to create new zone
+type CreateResponse struct {
+	ID    uint64 `json:"id,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+// RRSet dto as part of zone info from API
+type RRSet struct {
+	TTL     int              `json:"ttl"`
+	Records []ResourceRecord `json:"resource_records"`
+	Filters []RecordFilter   `json:"filters"`
+	Meta    *Meta            `json:"meta,omitempty"`
+}
+
+// ResourceRecord dto describe records in RRSet
+type ResourceRecord struct {
+	Content []interface{}          `json:"content"`
+	Meta    map[string]interface{} `json:"meta"`
+	Enabled bool                   `json:"enabled"`
+}
+
+// ContentToString as short value
+func (r ResourceRecord) ContentToString() string {
+	parts := make([]string, len(r.Content))
+	for i := range r.Content {
+		parts[i] = fmt.Sprint(r.Content[i])
+	}
+	return strings.Join(parts, " ")
+}
+
+// RecordFilter describe Filters in RRSet
+type RecordFilter struct {
+	Limit  uint   `json:"limit,omitempty"`
+	Type   string `json:"type"`
+	Strict bool   `json:"strict"`
+}
+
+// NewGeoDNSFilter for RRSet
+func NewGeoDNSFilter(limit uint, strict bool) RecordFilter {
+	return RecordFilter{
+		Limit:  limit,
+		Type:   "geodns",
+		Strict: strict,
+	}
+}
+
+// NewGeoDistanceFilter for RRSet
+func NewGeoDistanceFilter(limit uint, strict bool) RecordFilter {
+	return RecordFilter{
+		Limit:  limit,
+		Type:   "geodistance",
+		Strict: strict,
+	}
+}
+
+// NewDefaultFilter for RRSet
+func NewDefaultFilter(limit uint, strict bool) RecordFilter {
+	return RecordFilter{
+		Limit:  limit,
+		Type:   "default",
+		Strict: strict,
+	}
+}
+
+// NewFirstNFilter for RRSet
+func NewFirstNFilter(limit uint, strict bool) RecordFilter {
+	return RecordFilter{
+		Limit:  limit,
+		Type:   "first_n",
+		Strict: strict,
+	}
+}
+
+// NewIsHealthyFilter for RRSet
+func NewIsHealthyFilter(limit uint, strict bool) RecordFilter {
+	return RecordFilter{
+		Limit:  limit,
+		Type:   "is_healthy",
+		Strict: strict,
+	}
+}
+
+// RecordType contract
+type RecordType interface {
+	ToContent() []interface{}
+}
+
+// RecordTypeMX as type of record
+type RecordTypeMX string
+
+// ToContent convertor
+func (mx RecordTypeMX) ToContent() []interface{} {
+	parts := strings.Split(string(mx), " ")
+	// nolint: gomnd
+	if len(parts) != 2 {
+		return nil
+	}
+	content := make([]interface{}, len(parts))
+	// nolint: gomnd
+	content[1] = parts[1]
+	// nolint: gomnd
+	content[0], _ = strconv.ParseInt(parts[0], 10, 64)
+
+	return content
+}
+
+// RecordTypeCAA as type of record
+type RecordTypeCAA string
+
+// ToContent convertor
+func (caa RecordTypeCAA) ToContent() []interface{} {
+	parts := strings.Split(string(caa), " ")
+	// nolint: gomnd
+	if len(parts) < 3 {
+		return nil
+	}
+	content := make([]interface{}, len(parts))
+	// nolint: gomnd
+	content[1] = parts[1]
+	// nolint: gomnd
+	content[2] = strings.Join(parts[2:], " ")
+	// nolint: gomnd
+	content[0], _ = strconv.ParseInt(parts[0], 10, 64)
+
+	return content
+}
+
+// RecordTypeSRV as type of record
+type RecordTypeSRV string
+
+// ToContent convertor
+func (srv RecordTypeSRV) ToContent() []interface{} {
+	parts := strings.Split(string(srv), " ")
+	// nolint: gomnd
+	if len(parts) != 4 {
+		return nil
+	}
+	content := make([]interface{}, len(parts))
+	// nolint: gomnd
+	content[0], _ = strconv.ParseInt(parts[0], 10, 64)
+	// nolint: gomnd
+	content[1], _ = strconv.ParseInt(parts[1], 10, 64)
+	// nolint: gomnd
+	content[2], _ = strconv.ParseInt(parts[2], 10, 64)
+	// nolint: gomnd
+	content[3] = parts[3]
+
+	return content
+}
+
+// RecordTypeAny as type of record
+type RecordTypeAny string
+
+// ToContent convertor
+func (any RecordTypeAny) ToContent() []interface{} {
+	return []interface{}{string(any)}
+}
+
+// ToRecordType builder
+func ToRecordType(rType, content string) RecordType {
+	switch strings.ToLower(rType) {
+	case "mx":
+		return RecordTypeMX(content)
+	case "caa":
+		return RecordTypeCAA(content)
+	case "srv":
+		return RecordTypeSRV(content)
+	}
+	return RecordTypeAny(content)
+}
+
+// ContentFromValue convertor from flat value to valid for api
+func ContentFromValue(recordType, content string) []interface{} {
+	rt := ToRecordType(recordType, content)
+	if rt == nil {
+		return nil
+	}
+	return rt.ToContent()
+}
+
+// FailoverMeta
+type FailoverMeta struct {
+	Protocol       string `json:"protocol"`
+	Port           int    `json:"port"`
+	Frequency      int    `json:"frequency"`
+	Timeout        int    `json:"timeout"`
+	Method         string `json:"method,omitempty"`
+	Url            string `json:"url,omitempty"`
+	Tls            bool   `json:"tls,omitempty"`
+	Regexp         string `json:"regexp,omitempty"`
+	HTTPStatusCode int    `json:"http_status_code,omitempty"`
+	Host           string `json:"host,omitempty"`
+	Verify         bool   `json:"verify,omitempty"`
+}
+
+// ResourceMeta for ResourceRecord
+type ResourceMeta struct {
+	name     string
+	value    interface{}
+	validErr error
+}
+
+// Valid error
+func (rm ResourceMeta) Valid() error {
+	return rm.validErr
+}
+
+// NewResourceMetaIP for ip meta
+func NewResourceMetaIP(ips ...string) ResourceMeta {
+	for _, v := range ips {
+		ip := net.ParseIP(v)
+		if ip == nil {
+			// nolint: goerr113
+			return ResourceMeta{validErr: fmt.Errorf("wrong ip")}
+		}
+	}
+	return ResourceMeta{
+		name:  "ip",
+		value: ips,
+	}
+}
+
+// NewResourceMetaAsn for asn meta
+func NewResourceMetaAsn(asn ...uint64) ResourceMeta {
+	return ResourceMeta{
+		name:  "asn",
+		value: asn,
+	}
+}
+
+// NewResourceMetaLatLong for lat long meta
+func NewResourceMetaLatLong(latlong string) ResourceMeta {
+	latlong = strings.TrimLeft(latlong, "(")
+	latlong = strings.TrimLeft(latlong, "[")
+	latlong = strings.TrimLeft(latlong, "{")
+	latlong = strings.TrimRight(latlong, ")")
+	latlong = strings.TrimRight(latlong, "]")
+	latlong = strings.TrimRight(latlong, "}")
+	parts := strings.Split(strings.ReplaceAll(latlong, " ", ""), ",")
+	// nolint: gomnd
+	if len(parts) != 2 {
+		// nolint: goerr113
+		return ResourceMeta{validErr: fmt.Errorf("latlong invalid format")}
+	}
+	lat, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		// nolint: goerr113
+		return ResourceMeta{validErr: fmt.Errorf("lat is invalid: %w", err)}
+	}
+	long, err := strconv.ParseFloat(parts[1], 64)
+	// nolint: goerr113
+	if err != nil {
+		return ResourceMeta{validErr: fmt.Errorf("long is invalid: %w", err)}
+	}
+
+	return ResourceMeta{
+		name:  "latlong",
+		value: []float64{lat, long},
+	}
+}
+
+// NewResourceMetaNotes for notes meta
+func NewResourceMetaNotes(notes ...string) ResourceMeta {
+	return ResourceMeta{
+		name:  "notes",
+		value: notes,
+	}
+}
+
+// NewResourceMetaCountries for Countries meta
+func NewResourceMetaCountries(countries ...string) ResourceMeta {
+	return ResourceMeta{
+		name:  "countries",
+		value: countries,
+	}
+}
+
+// NewResourceMetaContinents for continents meta
+func NewResourceMetaContinents(continents ...string) ResourceMeta {
+	return ResourceMeta{
+		name:  "continents",
+		value: continents,
+	}
+}
+
+// NewResourceMetaDefault for default meta
+func NewResourceMetaDefault() ResourceMeta {
+	return ResourceMeta{
+		name:  "default",
+		value: true,
+	}
+}
+
+// SetContent to ResourceRecord
+func (r *ResourceRecord) SetContent(recordType, val string) *ResourceRecord {
+	r.Content = ContentFromValue(recordType, val)
+	return r
+}
+
+// AddMeta to ResourceRecord
+func (r *ResourceRecord) AddMeta(meta ResourceMeta) *ResourceRecord {
+	if meta.validErr != nil {
+		return r
+	}
+	if meta.name == "" || meta.value == "" {
+		return r
+	}
+	if r.Meta == nil {
+		r.Meta = map[string]interface{}{}
+	}
+	r.Meta[meta.name] = meta.value
+	return r
+}
+
+// AddFilter to RRSet
+func (rr *RRSet) AddFilter(filters ...RecordFilter) *RRSet {
+	if rr.Filters == nil {
+		rr.Filters = make([]RecordFilter, 0)
+	}
+	rr.Filters = append(rr.Filters, filters...)
+	return rr
+}
+
+// ZoneRecord dto describe records in Zone
+type ZoneRecord struct {
+	Name         string   `json:"name"`
+	Type         string   `json:"type"`
+	TTL          uint     `json:"ttl"`
+	ShortAnswers []string `json:"short_answers"`
+}
+
+// APIError customization for API calls
+type APIError struct {
+	StatusCode int    `json:"-"`
+	Message    string `json:"error,omitempty"`
+}
+
+// Error implementation
+func (a APIError) Error() string {
+	return fmt.Sprintf("%d: %s", a.StatusCode, a.Message)
+}
