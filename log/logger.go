0bb889720dc5621eb438a7c6e7cf16c4c61550b9
Artelhin <artelhin@gmail.com>
2025-10-29 16:44:08 +0300

    add logger with trace id

---
+++ log/logger.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	TraceIDKey = "trace_id"
+	ErrorKey   = "error"
+)
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// Trace adds tracing ID to ctx
+func Trace(ctx context.Context) context.Context {
+	traceId, err := uuid.NewRandom()
+	if err != nil {
+		logrus.Errorf("failed to generate UUID for tracing: %s", err)
+	}
+	return context.WithValue(ctx, TraceIDKey, traceId)
+}
+
+// Logger provides logger with embedded ctx in it
+func Logger(ctx context.Context) *logrus.Entry {
+	return logrus.StandardLogger().WithContext(ctx)
+}
